b1f3837879ceba5def7b753858b466876faaf029
miqueiasvitor14 <141414295+miqueiasvitor14@users.noreply.github.com>
2023-08-11 08:30:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+
+
+function setup() {
+ createCanvas(400, 400);
+background("pink");
+  cor = color(random(0,255),random(0,255),random(0,255));
+posicaoHorizontal = 100;
+posicaoVertical = 200;
+}
+
+function draw() {
+ 
+fill(cor);
+  circle(posicaoHorizontal, posicaoVertical,50);
+}
+
+
+
+
+if(mouseX < posicaoHorizontal -1)}
+    posicaoHorizontal = posicaoHorizontal +1
+  
